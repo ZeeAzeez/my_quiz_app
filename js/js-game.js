aa7b0fa7137eeaa13a88174cb4b283d01d65d359
ZeeAzeez <azeezahaazeez@gmail.com>
2024-01-16 13:50:08 +0100

    completed JS questions

--- js/js-game.js
+++ js/js-game.js
@@ -10,39 +10,39 @@ const questions = [
         ] 
     },
     {
-        question: "How do you select an element with the id 'example' in CSS",
+        question: "How do you declare a variable in JavaScript?",
         answers: [
-            { text: "A.  #example", correct: true },
-            { text: "B.  .example", correct: false },
-            { text: "C.  *example", correct: false },
-            { text: "D.  @example", correct: false }
+            { text: "A.  var myVar;", correct: false },
+            { text: "B.  variable myVar;", correct: false },
+            { text: "C.  v myVar;", correct: false },
+            { text: "D.  let myVar;", correct: true }
         ] 
     },
     {
-        question: "What property is used to set the text color in CSS",
+        question: "What will the following code output? console.log(2 + '2');",
         answers: [
-            { text: "A.   text-color ", correct: false },
-            { text: "B.   font-color", correct: false },
-            { text: "C.   color ", correct: true },
-            { text: "D.   text-style", correct: false }
+            { text: "A.   4 ", correct: false },
+            { text: "B.   22", correct: false },
+            { text: "C.   2 ", correct: false },
+            { text: "D.   '22'", correct: true }
         ]  
     },
     {
-        question: "Which of the following CSS values represents a transparent background?",
+        question: "How do you comment in JavaScript?",
         answers: [
-            { text: "A.  rgba(255, 255, 255, 0)", correct: false },
-            { text: "B.  #000000", correct: false },
-            { text: "C.  hsl(0, 100%, 50%)", correct: false },
-            { text: "D.  transparent", correct: true }
+            { text: "A.  // This is a comment", correct: true },
+            { text: "B.  < ! -- This is a comment -- > ", correct: false },
+            { text: "C.  /* This is a comment */", correct: false },
+            { text: "D.  # This is a comment", correct: false }
         ] 
     },
     {
-        question: "What is the default display property for a <div> element in CSS",
+        question: "What is the purpose of the else statement in JavaScript?",
         answers: [
-            { text: "A.  inline", correct: false },
-            { text: "B.  block", correct: true },
-            { text: "C.  inline-block", correct: false },
-            { text: "D.  flex", correct: false }
+            { text: "A.  To execute code if a condition is true", correct: false },
+            { text: "B.  To define a loop", correct: false },
+            { text: "C.  To execute code if a condition is false", correct: true },
+            { text: "D.  To declare a function", correct: false }
         ] 
     }
 ];
